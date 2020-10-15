ef31471f7e07a3241ea3502c648bc48beb717f03
Logan Leger <logan@loganleger.com>
2020-10-15 15:09:38 -0500

    Add .tool-versions to manage runtime versions with asdf

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixStarter.MixProject do
     [
       app: :phoenix_starter,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
