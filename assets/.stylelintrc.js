5df58c84a7bc0c65036b5591ba6380ec1d49e8d9
Logan Leger <logan@loganleger.com>
2020-10-15 16:53:19 -0500

    Add and configure Stylelint for linting CSS

---
+++ assets/.stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  plugins: ["stylelint-order"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["define-mixin", "mixin"],
+      },
+    ],
+    "declaration-colon-newline-after": "always-multi-line",
+    "declaration-empty-line-before": "never",
+    "order/properties-alphabetical-order": true,
+  },
+}
