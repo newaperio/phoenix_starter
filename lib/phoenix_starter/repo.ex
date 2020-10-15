ea3612f00a13bfd9e77b39431b82d586fe8f18ef
Logan Leger <logan@loganleger.com>
2020-10-15 15:32:06 -0500

    Add Repo/Schema add-ons
    - Add a Schema module to be used instead of Ecto.Schema to set some
      defaults
    - Add a type for Ecto return values to Repo module for convenience

--- lib/phoenix_starter/repo.ex
+++ lib/phoenix_starter/repo.ex
@@ -2,4 +2,14 @@ defmodule PhoenixStarter.Repo do
   use Ecto.Repo,
     otp_app: :phoenix_starter,
     adapter: Ecto.Adapters.Postgres
+
+  @typedoc """
+  Represents what can be expected as the result of an Ecto operation on a
+  changeset.
+  """
+  @type result() ::
+          {:ok, Ecto.Schema.t()}
+          | {:ok, %{required(Ecto.Multi.name()) => Ecto.Schema.t()}}
+          | {:error, Ecto.Changeset.t()}
+          | {:error, Ecto.Multi.name(), any(), %{required(Ecto.Multi.name()) => any()}}
 end
---
+++ lib/phoenix_starter/schema.ex
@@ -0,0 +1,11 @@
+defmodule PhoenixStarter.Schema do
+  defmacro __using__(_opts) do
+    quote do
+      use Ecto.Schema
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts [type: :utc_datetime]
+    end
+  end
+end
