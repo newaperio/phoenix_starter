e86ff6669081abfd6eb6998d12207c72b21ea8c6
Logan Leger <logan@loganleger.com>
2020-10-16 16:11:38 -0500

    Add and configure PostCSS
    * Add autoprefixer to add browser prefixes automatically
    * Add postcss-nested to enable SASS style nested rules
    * Add postcss-import to allow @import statements

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (env, options) => {
         },
         {
           test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
         },
       ],
     },
