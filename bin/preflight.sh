b5b20f3c18ffa3dd5afbf6b6123a2c223a322c3f
Logan Leger <logan@loganleger.com>
2020-10-30 16:33:53 -0500

    Add preflight script to check if all system deps are installed
    This is a helpful script to check dev envs to make sure all required
    system deps are present.

---
+++ bin/preflight.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+check_failed=0
+
+check_executable() {
+  if ! [ -x "$(command -v "$1")" ]; then
+    print_error "Could not find \`$1\` exec"
+    check_failed=$((check_failed + 1))
+  fi
+}
+
+print_step() {
+  echo
+  echo "==> $1"
+}
+
+print_error() {
+  echo -e "\033[31m! Error:\033[0m $1"
+}
+
+print_success() {
+  echo -e "\033[32m> Success:\033[0m $1"
+}
+
+print_step "Checking prerequisites"
+
+check_executable asdf
+check_executable docker
+check_executable psql
+
+if [[ $check_failed = 0 ]]; then
+  print_success "All prerequisites installed"
+
+  print_step "Running asdf..."
+  asdf install
+
+  print_success "Preflight finished"
+else
+  exit 1
+fi
