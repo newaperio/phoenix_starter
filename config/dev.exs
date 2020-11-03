16c69c8745b9cbb045d0ce61d823e4852af76b5c
Nicholas Scheurich <nick@scheurich.me>
2020-11-03 10:40:10 -0600

    Update webpack watch mode flag
     `--watch-stdin` is incorrect as of the currently-specified version of
     webpack.

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :phoenix_starter, PhoenixStarterWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
