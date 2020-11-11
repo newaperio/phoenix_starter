6ea49f852887e578773a57a39a89efa9043d313a
Logan Leger <logan@loganleger.com>
2020-11-10 20:53:58 -0600

    Improve docs w/r/t to prereqs that need to be installed

--- bin/preflight.sh
+++ bin/preflight.sh
@@ -26,12 +26,19 @@ print_success() {
 
 print_step "Checking prerequisites"
 
+check_executable autoconf # Erlang prereq
+check_executable gpg # Node prereq
 check_executable asdf
 check_executable docker
 check_executable psql
 
 if [[ $check_failed = 0 ]]; then
   print_success "All prerequisites installed"
+
+  print_step "Installing asdf plugins..."
+  asdf plugin-add erlang
+  asdf plugin-add elixir
+  asdf plugin-add nodejs
 
   print_step "Running asdf..."
   asdf install
