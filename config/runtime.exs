137e2dfd47478a66de415c2a41d5f51e845b51d0
Logan Leger <logan@loganleger.com>
2020-12-09 11:03:08 -0600

    Update config

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,36 +5,54 @@ if config_env() == :prod do
   config :phoenix_starter, PhoenixStarter.Repo,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-  # if database_url == nil do
-  #   raise """
-  #   environment variable DATABASE_URL is missing.
-  #   For example: ecto://USER:PASS@HOST/DATABASE
-  #   """
-  # end
+  with {:ok, database_name} <- System.fetch_env("DATABASE_NAME"),
+       {:ok, database_username} <- System.fetch_env("DATABASE_USER"),
+       {:ok, database_password} <- System.fetch_env("DATABASE_PASSWORD"),
+       {:ok, database_hostname} <- System.fetch_env("DATABASE_HOST") do
+    config :phoenix_starter, PhoenixStarter.Repo,
+      database: database_name,
+      username: database_username,
+      password: database_password,
+      hostname: database_hostname
+  else
+    _ ->
+      raise """
+      environment variables for database missing.
+      Check that all the following are defined:
+        - DATABASE_NAME
+        - DATABASE_USER
+        - DATABASE_PASSWORD
+        - DATABASE_HOST
+      """
+  end
 
-  # secret_key_base =
-  #   System.get_env("SECRET_KEY_BASE") ||
-  #     raise """
-  #     environment variable SECRET_KEY_BASE is missing.
-  #     You can generate one by calling: mix phx.gen.secret
-  #     """
+  case System.fetch_env("DATABASE_URL") do
+    :error ->
+      config :phoenix_starter, PhoenixStarter.Repo,
+        database: System.fetch_env!("DATABASE_NAME"),
+        username: System.fetch_env!("DATABASE_USER"),
+        password: System.fetch_env!("DATABASE_PASSWORD"),
+        hostname: System.fetch_env!("DATABASE_HOST")
 
-  # config :phoenix_starter, PhoenixStarterWeb.Endpoint,
-  #   http: [
-  #     port: String.to_integer(System.get_env("PORT") || "4000"),
-  #     transport_options: [socket_opts: [:inet6]]
-  #   ],
-  #   live_view: [signing_salt: System.get_env("LIVE_VIEW_SALT")],
-  #   secret_key_base: secret_key_base,
-  #   url: [scheme: "https", host: System.get_env("APP_HOST"), port: 443]
+    {:ok, database_url} ->
+      config :phoenix_starter, PhoenixStarter.Repo, url: database_url
+  end
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
 
   config :phoenix_starter, PhoenixStarterWeb.Endpoint,
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
     ],
+    live_view: [signing_salt: System.fetch_env!("LIVE_VIEW_SALT")],
     secret_key_base: secret_key_base,
-    url: [scheme: "https", port: 443]
+    url: [scheme: "https", host: System.get_env("APP_HOST"), port: 443]
 
   # Configures Bamboo
   # Note: by default this reads from the IAM task or instance role
@@ -46,14 +64,13 @@ if config_env() == :prod do
   #   environment_name: System.fetch_env!("SENTRY_ENVIRONMENT")
 end
 
-case System.fetch_env("DATABASE_URL") do
-  :error ->
-    config :phoenix_starter, PhoenixStarter.Repo,
-      database: System.fetch_env!("DATABASE_NAME"),
-      username: System.fetch_env!("DATABASE_USER"),
-      password: System.fetch_env!("DATABASE_PASSWORD"),
-      hostname: System.fetch_env!("DATABASE_HOST")
+if config_env() == :test && System.get_env("CI", false) do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
 
-  {:ok, database_url} ->
-    config :phoenix_starter, PhoenixStarter.Repo, url: database_url
+  config :phoenix_starter, PhoenixStarter.Repo, url: database_url
 end
