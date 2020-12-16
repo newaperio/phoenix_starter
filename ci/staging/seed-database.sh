eb2180e0b0a2548d015706dc2b629177c3517981
Eric Duvic <ericduvic@gmail.com>
2020-12-16 01:54:10 -0700

    Add script for seeding DB

---
+++ ci/staging/seed-database.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+APP_NAME="phoenix-starter"
+
+# Get service networkConfiguration
+AWSVPC_CONFIG=$(aws ecs describe-services --services "${APP_NAME}" --cluster "${APP_NAME}" | jq -c ".services[] | select( .serviceName | contains(\"$APP_NAME\") ) | .networkConfiguration.awsvpcConfiguration" | tr -d \" | tr ':' '=')
+
+aws ecs run-task --cluster "${APP_NAME}" --task-definition phoenix-starter-seed --network-configuration "awsvpcConfiguration=${AWSVPC_CONFIG}"
