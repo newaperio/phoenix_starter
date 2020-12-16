7a234a217d012a0f39eed2600afd9215766edf5f
Eric Duvic <ericduvic@gmail.com>
2020-12-16 01:56:25 -0700

    Relocate bootstrap

--- infrastructure/modules/bootstrap/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "region" {
-    value = var.region
-}
--- infrastructure/modules/bootstrap/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-    type = string
-    description = "The AWS region"
-}
-
-variable "aws_profile" {
-  description = "Name of the AWS Profile to use. Equivalent of the AWS_PROFILE env variable."
-}
