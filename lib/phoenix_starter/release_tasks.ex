99bfb4e0d9f4859e54735e374dac8cf0fb1c660a
Logan Leger <logan@loganleger.com>
2020-12-16 19:34:33 -0600

    Fix seeds task

--- lib/phoenix_starter/release_tasks.ex
+++ lib/phoenix_starter/release_tasks.ex
@@ -40,10 +40,11 @@ defmodule PhoenixStarter.ReleaseTasks do
 
     for repo <- repos() do
       {:ok, _, _} =
-        Ecto.Migrator.with_repo(repo, fn _ ->
+        Ecto.Migrator.with_repo(repo, fn repo ->
           seeds_path = Ecto.Migrator.migrations_path(repo, "/seeds.ex")
 
           if File.exists?(seeds_path) do
+            Logger.info("Running seeds for #{repo}: #{seeds_path}")
             Code.eval_file(seeds_path)
           end
         end)
