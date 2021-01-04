2b1f4698293e2b612b5188ba1be6e51a57fe61a7
Eric Duvic <ericduvic@gmail.com>
2021-01-03 19:40:20 -0700

    Update scripts to use new task security group

--- ci/staging/seed-database.sh
+++ ci/staging/seed-database.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 source ../../bin/env.sh
 
 # Get service networkConfiguration
-AWSVPC_CONFIG=$(aws ecs describe-services --services "${APP_NAME}" --cluster "${APP_NAME}" | jq -c ".services[] | select( .serviceName | contains(\"$APP_NAME\") ) | .networkConfiguration.awsvpcConfiguration" | tr -d \" | tr ':' '=')
+AWSVPC_SUBNETS=$(aws ecs describe-services --services "${APP_NAME}" --cluster "${APP_NAME}" | jq -c ".services[] | select( .serviceName | contains(\"$APP_NAME\") ) | .networkConfiguration.awsvpcConfiguration.subnets" | tr -d \")
 
-aws ecs run-task --cluster "${APP_NAME}" --task-definition "${APP_NAME}-seed" --network-configuration "awsvpcConfiguration=${AWSVPC_CONFIG}"
+pushd ../../infrastructure/staging
+  AWSVPC_SECURITY_GROUP=$(terraform output task_security_group)
+popd
+
+AWSVPC_CONFIG="awsvpcConfiguration={subnets=${AWSVPC_SUBNETS},securityGroups=[${AWSVPC_SECURITY_GROUP}],assignPublicIp=DISABLED}"
+
+aws ecs run-task --cluster "${APP_NAME}" --task-definition "${APP_NAME}-seed" --network-configuration "${AWSVPC_CONFIG}"
