6de6dbaef73ac8fbdd039d73d1c47211d8868d4f
Logan Leger <logan@loganleger.com>
2022-07-29 22:01:17 -0500

    Add `mix lint` to run all steps from CI lint step locally

--- mix.exs
+++ mix.exs
@@ -79,7 +79,14 @@ defmodule PhoenixStarter.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      lint: [
+        "deps.unlock --check-unused",
+        "format --check-formatted",
+        "xref graph --label compile-connected --fail-above 0",
+        "credo",
+        "sobelow --config"
+      ]
     ]
   end
 
